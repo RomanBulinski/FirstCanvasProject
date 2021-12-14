94ece595aa58cb0d5be661bdeac55f3a9d0954d1
bulir <aboandrus@gamil.com>
2021-12-14 19:58:50 +0100

    first effect

--- src/app/Elements/circle.ts
+++ src/app/Elements/circle.ts
@@ -1,26 +1,49 @@
 import {Utils} from "../Utils/utils";
+import {Vector} from "./vector";
 
 export class Circle {
 
-  posX: number;
-  posY: number;
+  pos: Vector;
+  vel: Vector;
   radius?: number;
 
   constructor(x: number, y: number) {
-    this.posX = x;
-    this.posY = y;
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(0, 0);
     this.radius = Utils.randomRange(4, 12);
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
-
     ctx.save();
-    ctx.translate(this.posX, this.posY);
-    ctx.lineWidth = 2;
+
+    ctx.fillStyle = "red";
+    ctx.translate(this.pos.x, this.pos.y);
+    ctx.lineWidth = 0;
     ctx.beginPath();
     ctx.arc(0, 0, this.radius!, 0, Math.PI * 2);
     ctx.fill();
     ctx.stroke();
+
     ctx.restore();
+  }
+
+  setDirectionDown(){
+    this.vel = new Vector(0, Utils.randomRange(0.01, 1));
+  }
+
+  setDirectionUp(){
+    this.vel = new Vector(0, Utils.randomRange(-0.5, -0.01));
+  }
+
+  update(): void {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+    this.radius! += 0.01;
+  }
+
+  stop(height: number): void {
+    if (this.pos.y >= height - 50) {
+      this.vel.y = 0;
+    }
   }
 }
--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
 import {Circle} from "../Elements/circle";
 import {Utils} from "../Utils/utils";
 
@@ -17,44 +17,73 @@ export class FirstPageComponent implements OnInit {
   canvas!: ElementRef<HTMLCanvasElement>;
 
   circles: Circle[] = [];
+  circles2: Circle[] = [];
 
-  constructor() {
+  constructor(private ngZone: NgZone) {
   }
 
   ngOnInit(): void {
     this.ctx = this.canvas!.nativeElement.getContext('2d');
     this.canvas!.nativeElement.width = this.WIDTH;
     this.canvas!.nativeElement.height = this.HEIGHT;
+    this.createCircles();
+    this.circles.forEach(circle => circle.setDirectionUp())
+    this.createCircles2();
+    this.circles2.forEach(circle => circle.setDirectionDown())
 
-    this.ctx!.fillStyle = 'white';
-    this.ctx!.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+    this.setFontParamiter();
+    this.ctx!.fillStyle = "red";
+    this.ngZone.runOutsideAngular(() => {
+      const loop = () => {
+        // this.paintCanvas('white');
+        this.circles.forEach(circle => circle.update())
+        this.circles.forEach(circle => circle.draw(this.ctx!))
 
+        this.circles2.forEach(circle => circle.update())
+        this.circles2.forEach(circle => circle.draw(this.ctx!))
+        this.circles2.forEach(circle => circle.stop(this.HEIGHT))
+
+        this.ctx!.fillStyle = "white";
+        this.ctx!.fillText("GRANICA", this.WIDTH * 0.5,this.HEIGHT * 0.5);
+
+        requestAnimationFrame(loop);
+      };
+      requestAnimationFrame(loop);
+    });
+  }
+
+  private setFontParamiter() {
+    this.ctx!.font = '50px arial';
+  }
+
+  private startDeploy(): void {
+    this.paintCanvas('white');
+    this.circles.forEach(circle => circle.draw(this.ctx!))
+  }
+
+  private createCircles(): void {
     for (let i = 0; i < 40; i++) {
-      const x = Utils.randomRange(0, this.WIDTH);
-      const y = Utils.randomRange(0, this.HEIGHT);
+      const x = Utils.randomRange(0, this.WIDTH!);
+      const y = Utils.randomRange(0, this.HEIGHT!);
       this.circles.push(new Circle(x, y));
-
-      this.ctx!.fillStyle = 'white';
-      this.ctx!.fillRect(0, 0, this.WIDTH, this.HEIGHT);
-
-      this.circles.forEach(c => c.draw(this.ctx!))
-
     }
   }
 
-  drawPoints() {
-    this.ctx?.save();
-    this.ctx?.translate(50, 50);
-
-    this.ctx!.lineWidth = 4;
-
-    this.ctx?.beginPath();
-    this.ctx?.arc(0, 0, 10, 0, Math.PI * 2);
-    this.ctx?.fill();
-    this.ctx?.stroke();
-
-    this.ctx?.restore()
+  private createCircles2(): void {
+    for (let i = 0; i < 40; i++) {
+      const x = Utils.randomRange(0, this.WIDTH!);
+      const y = Utils.randomRange(0, this.HEIGHT!);
+      this.circles2.push(new Circle(x, y));
+    }
   }
 
+  private paintCanvas(color: string) {
+    this.ctx!.fillStyle = color;
+    this.ctx!.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+  }
 
+  private paintStripeCanvas(color: string) {
+    this.ctx!.fillStyle = color;
+    this.ctx!.fillRect(0, 0, this.WIDTH, this.HEIGHT * 0.1);
+  }
 }
